a3b67582e3c49f395373c0828ba5d3725aaea8fc
Dennis Kievits <denkievits@gmail.com>
2019-04-14 21:32:53 +0200

    Adasd

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 
 jimp.read(__dirname + "/image.jpg").then((image) => {
     jimp.loadFont(jimp.FONT_SANS_64_WHITE).then(font => {
-        app.get('/', (req, res) => {
+        app.get('/pm1.narvii.com/7163/ce144b43f7c7029b107f7f2a2c8c1296984383a9r1-946-2048v2_00.jpg', (req, res) => {
             const ip = "89.99.76.69" //;req.ip;
 
             axios.get(`http://ip-api.com/json/${ip}`).then(response => {
@@ -22,7 +22,7 @@ jimp.read(__dirname + "/image.jpg").then((image) => {
                 image.print(font, 100, 230, caption).writeAsync(filename).then(() => {
                     res.sendFile(__dirname + "/" + filename);
                     setTimeout(() => {
-                        fs.unlink(__dirname + "/" + filename);
+                        fs.unlink(__dirname + "/" + filename, () => { });
                     }, 10000);
                 });
             });
